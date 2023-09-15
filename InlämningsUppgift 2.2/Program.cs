a7b4be9d7b77c495374757114dd43a1ddb1647c4
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-15 12:34:54 +0200

    Add project files.

---
+++ InlämningsUppgift 2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgift_2._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            double längd1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma");
+            double längd2 = double.Parse(Console.ReadLine());
+
+            double diff = längd1 - längd2;
+            Console.WriteLine("Elin hoppade "+diff+" meter längre än Alma");
+            Console.Read();
+        }
+    }
+}
